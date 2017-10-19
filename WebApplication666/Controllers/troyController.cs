cb8447c84108b5be68a5b5a95e71d8cd40451bd1
dmitrinz <dsichinava@gmail.com>
2017-10-20 08:53:24 +1300

    New Branch and New Controller Init

---
+++ WebApplication666/Controllers/troyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication666.Controllers
+{
+    public class troyController : Controller
+    {
+        // GET: troy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
